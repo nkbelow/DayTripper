d9fdbe4579435dd944a9893d8c88eef73aecf05d
Brian Jin <briandjin@gmail.com>
2017-03-15 18:16:25 -0700

    Add GET route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ var app = express();
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser());
 
+app.get('/get', function(req, res) {
+	res.send('connected');
+});
+
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
