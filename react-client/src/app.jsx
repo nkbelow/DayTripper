33addeb7763559b967f984b4c69c9fc2f287d48d
Brian Jin <brianjin@Brians-MacBook-Air-2.local>
2017-03-21 20:18:13 -0700

    Remove dummy data and call getEvents function in constructor

--- react-client/src/app.jsx
+++ react-client/src/app.jsx
@@ -5,41 +5,6 @@ import Search from './components/search.jsx';
 import MapView from './components/map.jsx';
 import EventList from './components/event-list.jsx';
 
-var dummyData = [
-  {
-  description: 'Breakfast with Fred',
-  start: '9am',
-  end: '11am',
-  location: 'Omelette House',
-  phone: '415-111-1111',
-  address: '111 Street St San Francisco, CA 94102',
-  latitude: 34.45,
-  longitude: 31.45
-  },
-
-  {
-  description: 'Greenfield with Beth',
-  start: '1pm',
-  end: '4pm',
-  location: '7Leaves Cafe',
-  phone: '415-111-2222',
-  address: '222 Holly St San Francisco, CA 94102',
-  latitude: 88.45,
-  longitude: 38.45
-  },
-
-  {
-  description: 'Clubbing with Marcus!',
-  start: '10pm',
-  end: '2am',
-  location: 'Temple',
-  phone: '415-333-4444',
-  address: '333 Street St San Francisco, CA 94106',
-  latitude: 89.45,
-  longitude: 77.45
-  }
-]
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -48,6 +13,8 @@ class App extends React.Component {
     }
     this.getEvents = this.getEvents.bind(this);
     this.createEvent = this.createEvent.bind(this);
+
+    this.getEvents();
   };
 
   getEvents() {
