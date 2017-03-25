f733c4020c90ba17c549f2c35387a96c3f94b681
Allen An Le <allenanle16@gmail.com>
2017-03-24 17:31:23 -0700

    FRONT-END: 'Edit' feature for every events

--- react-client/src/components/event-entry.jsx
+++ react-client/src/components/event-entry.jsx
@@ -4,16 +4,34 @@ class EventEntry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ifRemoveEvent: false,
+      ifOptions: false,
+      ifEditEvent: false
     }
-    this.ifRemoveEvent = this.ifRemoveEvent.bind(this);
+    this.ifOptions = this.ifOptions.bind(this);
+    this.ifEditEvent = this.ifEditEvent.bind(this);
     this.onRemoveEvent = this.onRemoveEvent.bind(this);
+    this.onUpdateEvent = this.onUpdateEvent.bind(this);
   };
 
-  ifRemoveEvent() {
+  ifOptions() {
     this.setState({
-      ifRemoveEvent: !this.state.ifRemoveEvent
+      ifOptions: !this.state.ifOptions
     })
+  };
+
+  ifEditEvent() {
+    this.setState({
+      ifEditEvent: !this.state.ifEditEvent
+    })
+  };
+
+  onUpdateEvent() {
+    console.log('EVENT UPDATED!')
+
+    // console.log('DELETED EVENT:', this.props.events[this.props.index]);
+
+    // const selectedEvent = this.props.events[this.props.index];
+    // this.props.updateEvent(selectedEvent)
   };
 
   onRemoveEvent() {
@@ -23,31 +41,109 @@ class EventEntry extends React.Component {
     this.props.removeEvent(selectedEvent)
   };
 
-  render () {
-    if (this.state.ifRemoveEvent) {
+  render() {
+    var timeBox = {
+      width: 70,
+      marginRight: 10,
+      marginLeft: 10,
+      textAlign: 'center',
+      fontFamily: 'Century Gothic',
+      fontSize: 17,
+      border: 0,
+      outline: 0,
+      background: 'transparent',
+      borderBottom: '1px solid black',
+      display: 'inline-block'
+    };
+
+    var descBox = {
+      width: 300,
+      marginRight: 15,
+      marginLeft: 5,
+      textAlign: 'center',
+      fontFamily: 'Century Gothic',
+      fontSize: 17,
+      border: 0,
+      outline: 0,
+      background: 'transparent',
+      borderBottom: '1px solid black',
+      display: 'inline-block'
+    };
+
+
+    if (this.state.ifEditEvent) {
       return (
-        <div id='event-entry' onClick={this.ifRemoveEvent}>
-          <li>
+        <li>
+          <div onClick={this.ifOptions}>
             {this.props.event.description}
-              <div>
-                {this.props.event.start} – {this.props.event.end} at {this.props.event.location}
-              </div>
-              <div>
-                <button onClick={this.onRemoveEvent}>Remove</button>
-              </div>
-          </li>
-        </div>
+            <div>
+            {this.props.event.start} – {this.props.event.end} at {this.props.event.location}
+            </div>
+          </div>
+
+          <div>
+            <button onClick={this.onRemoveEvent}>REMOVE</button>
+            <button onClick={this.ifEditEvent}>EDIT</button>
+          </div>
+
+          <div>
+            description:
+            <input
+              style={descBox}
+              type="text"
+              onChange={this.onDescChange}
+            />
+          </div>
+
+          <div>
+            <input
+              placeholder='start'
+              style={timeBox}
+              type="text"
+              onChange={this.onStartChange}
+            />
+
+            |
+            <input
+              placeholder='end'
+              style={timeBox}
+              type="text"
+              onChange={this.onEndChange}
+            />
+
+            <button onClick={this.onUpdateEvent}>UPDATE</button>
+          </div>
+        </li>
       )
+    };
+
+    if (this.state.ifOptions) {
+      return (
+        <li>
+          <div onClick={this.ifOptions}>
+            {this.props.event.description}
+            <div>
+            {this.props.event.start} – {this.props.event.end} at {this.props.event.location}
+            </div>
+          </div>
+
+          <div>
+            <button onClick={this.onRemoveEvent}>REMOVE</button>
+            <button onClick={this.ifEditEvent}>EDIT</button>
+          </div>
+        </li>
+      )
+
     } else {
       return (
-        <div id='event-entry' onClick={this.ifRemoveEvent}>
-          <li>
+        <li>
+          <div onClick={this.ifOptions}>
             {this.props.event.description}
-              <div>
-                {this.props.event.start} – {this.props.event.end} at {this.props.event.location}
-              </div>
-          </li>
-        </div>
+            <div>
+            {this.props.event.start} – {this.props.event.end} at {this.props.event.location}
+            </div>
+          </div>
+        </li>
       )
     }
   };
