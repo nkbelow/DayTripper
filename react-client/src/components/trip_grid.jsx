5592e8b9d0652571f8f19713476b18e969d4022b
Anthony Bianco <anthony.bianco3@gmail.com>
2017-03-29 14:44:28 -0700

    create trip grid component

---
+++ react-client/src/components/trip_grid.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TripGridTile from './trip_grid_tile.jsx';
+
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  gridList: {
+    width: '75%',
+    overflowY: 'auto',
+    margin: '0 auto'
+  },
+};
+
+class TripGridList extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    let trips = [];
+    for (let i = 0; i < 10; i++) {
+      trips.push({
+        id: i,
+        name: 'Pizza Pizza Pizza!',
+        image_url: 'http://www.seriouseats.com/images/2015/01/20150127-san-francisco-pizza-by-the-slice-primary.jpg'
+      });
+    }
+    this.state = {
+      trips: trips
+    }
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div style={styles.root}>
+          <GridList
+            style={styles.gridList}
+            cellHeight={300}
+          >
+            {this.state.trips.map((result) => (
+              <TripGridTile 
+                key={result.id}
+                result={result}
+              />
+            ))}
+          </GridList>
+        </div>
+      </MuiThemeProvider>
+    ) 
+  };
+}
+
+export default TripGridList;
