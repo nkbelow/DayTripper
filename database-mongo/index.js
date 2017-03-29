9fc4c359492a9f5d790f7e9a54d6c392835cf425
Anthony Bianco <anthony.bianco3@gmail.com>
2017-03-28 19:53:48 -0700

    set up mongodb addon

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/test');
-mongoose.connect('mongodb://mm:hrsf73@ds159348.mlab.com:59348/day-tripper');
+//mongoose.connect('mongodb://mm:hrsf73@ds159348.mlab.com:59348/day-tripper');
+mongoose.connect(process.env.MONGODB_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
